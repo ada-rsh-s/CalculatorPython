f2d47c685943d0a3f925cc3138f13145bb9e7155
Adarsh S <64910179+Adarsh-S1@users.noreply.github.com>
2020-06-27 14:09:39 +0530

    Entry widget placed

--- Calculator.py
+++ Calculator.py
@@ -4,15 +4,12 @@ window = Tk()
 
 windowWidth = window.winfo_reqwidth()
 windowHeight = window.winfo_reqheight()
-print("Width", windowWidth, "Height", windowHeight)
-
 positionRight = int(window.winfo_screenwidth() / 2.25 - windowWidth / 1)
 positionDown = int(window.winfo_screenheight() / 2.8 - windowHeight / 1)
-
 window.geometry("+{}+{}".format(positionRight, positionDown))
 window.geometry("500x540")
 window.title("CALCULATOR")
-window.configure(bg="lightgrey")
-label_widget = Label(window, text="A Label", font=("Helvetica", 16), width=540, height=2)
-label_widget.pack()
+window.configure(bg="light grey")
+label_widget = Entry(window, bg="#7f8785", fg="white", font=("Helvetica", 16))
+label_widget.grid(ipady=16, ipadx=110,padx=18,pady=30)
 window = mainloop()
