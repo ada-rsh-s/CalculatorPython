bac35b200d1c2f47a11de7e43a85a2e5b3096aa6
Adarsh S <64910179+Adarsh-S1@users.noreply.github.com>
2020-06-27 12:09:35 +0530

    window centralised

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,15 @@
 from tkinter import *
 
 window = Tk()
+
+windowWidth = window.winfo_reqwidth()
+windowHeight = window.winfo_reqheight()
+print("Width", windowWidth, "Height", windowHeight)
+
+positionRight = int(window.winfo_screenwidth() / 2.25 - windowWidth / 1)
+positionDown = int(window.winfo_screenheight() / 2.8 - windowHeight / 1)
+
+window.geometry("+{}+{}".format(positionRight, positionDown))
 window.geometry("500x540")
 window.title("CALCULATOR")
 window.configure(bg="lightgrey")
